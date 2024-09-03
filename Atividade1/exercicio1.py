babb3502217cfc9a45af6871bbde867086e0f4df
Pablo Ricardo <pabloricardo055@gmail.com>
2024-09-03 17:11:03 -0300

    Realização da Atividade 1

---
+++ Atividade1/exercicio1.py
@@ -0,0 +1,8 @@
+idade = int(input('Digite sua idade:'))
+mes = 12
+dia = 365
+
+idadeM = idade*mes
+idadeD = idade*dia
+
+print(f'Sua idade em meses: {idadeM} meses \nSua idade em dias: {idadeD} dias')
---
+++ Atividade2/exercicio2.py
@@ -0,0 +1,6 @@
+total = float(input('Quantos litros de suco deseja fazer? '))
+
+qtdAgua = 0.8*total
+qtdSuco = 0.2*total
+
+print(f'Para produzir a quantidade de {total} litros, você precisa de {qtdAgua:.2f}L e {qtdSuco:.2f}L de Suco de Maracujá!')
