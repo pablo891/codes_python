e39d26a19c1fc82ad6e9262f97e54fe58f62a6b4
Pablo Ricardo <pabloricardo055@gmail.com>
2024-09-04 15:45:58 -0300

    Realização da Atividade 2

--- Atividade2/exercicio2.py
+++ Atividade2/exercicio2.py
@@ -3,4 +3,4 @@ total = float(input('Quantos litros de suco deseja fazer? '))
 qtdAgua = 0.8*total
 qtdSuco = 0.2*total
 
-print(f'Para fazer {total} litros de Refresco, você precisa de {qtdAgua:.1f}L e {qtdSuco:.1f}L de Suco de Maracujá!')
+print(f'Para fazer {total} litros de Refresco, você precisa de {qtdAgua:.1f}L e {qtdSuco:.1f}L de Suco de Maracujá!') 
