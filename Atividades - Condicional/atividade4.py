80f84c145f7b8bdddefbfcd907b4f1b4ae3280e2
Pablo Ricardo <pabloricardo055@gmail.com>
2024-09-09 13:45:45 -0300

    Modificação da Atividade 4

--- Atividades - Condicional/atividade4.py
+++ Atividades - Condicional/atividade4.py
@@ -3,6 +3,6 @@ valor = int(input('Digite um valor: '))
 if valor%2 == 0:
     soma = valor +10
     print(f'Resultado da soma: {soma}')
-elif valor%2 != 0:
+elif valor%2 != 1:
     mult = valor * 2
     print(f'Resultado da multiplicação: {mult}')
